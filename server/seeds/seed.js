7e04a59f52e37fd800db45a9c04c632603dd9564
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-03 20:00:54 -0500

    fixed db errors and seeded db

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,12 +2,13 @@ const db = require('../config/connection');
 const { Contact } = require('../models/Contact');
 const cleanDB = require('./cleanDB')
 
-const contactData = require('./contactData.json');
+const contactData = require('./contactData.json')
+console.log(contactData)
 
 db.once('open', async () => {
     // await cleanDB('Contact', 'contact');
 
-    await db.collection(Contact).insertMany(contactData);
+    await db.collection('Contact').insertMany(contactData);
 
     console.log('Test contacts seeded!')
     process.exit(0);
