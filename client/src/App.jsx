98126d9e18c680ff4158484d6880056a3ee51d45
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-03 20:20:23 -0500

    frontend basics

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Header from './components/Header';
+import './App.css'
+
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+});
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <div className="flex-column justify-flex-start min-100-vh">
+                <Header />
+                <div className="container">
+                    <Outlet />
+                </div>
+            </div>
+        </ApolloProvider>
+    )
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import '../src/index.css'
+
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true, 
+                element: <Home />
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />
+)
