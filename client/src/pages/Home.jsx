6caab8e30875fb8b6ffb92dfb1390a2a9754a4c5
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-08 17:38:48 -0500

    added about section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,20 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <>
-        <p>
-            Hello! 
-        </p>
-           
+            <div className="about-card">
+                <p>
+                    Hello! Thank you for visiting my portfolio. I am Elizabeth Tucker, a Junior Web Developer and recent graduate of the University of Texas at Austin's Full-Stack MERN Coding Bootcamp.
+                    <br />
+                    <br />
+                    Below, you will find a selection of projects I worked on for the course, as well as personal projects (coming soon!)
+                    <br />
+                    <br />
+                    Prior to my time at UTA, I worked in Operations and IT for a small import company. I was also an Adult Programming Librarian at the Henry Carter Hull Library in Clinton, Connecticut. I do not have any formal IT training, but my time in both of those positions gave me a wealth of knowledge-- and a number of strategies to seek the answers that I don't have on hand.
+                    <br />
+                    <br />
+                    Thanks for stopping by! I look forward to hearing from you.
+                </p>
+            </div>
         </>
     )
 }
