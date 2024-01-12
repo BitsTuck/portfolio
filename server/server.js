9053d3e29623854ba49f7f674391928730efc4d4
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-12 15:34:51 -0500

    still trying to debug the server

--- server/server.js
+++ server/server.js
@@ -20,24 +20,20 @@ const startApolloServer = async () => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    app.use('/graphql', expressMiddleware(server, {
-        context: authMiddleware
-    }));
-
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/dist')));
 
         app.get('*', (req, res) => {
+            console.log('wildcard route is active')
             res.sendFile(path.join(__dirname, '../client/dist/index.html'));
         });
     }
+    
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
 
   
-
-
-
-
-
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server is running on port ${PORT}`);
