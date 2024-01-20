f83167e7ab44638a8ac187835fda0a97515df78f
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-20 11:24:20 -0500

    connection file update for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,4 +4,9 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bitstucker:p0rtfo1iOv1@cluster0.hvkzmuy.mongodb.net/')
 
+const db=mongoose.connection;
+db.on('error', console.error.bind(console,'MongoDB connection error'));
+db.once('open', () => {
+    console.log('Connected to MongoDB Atlas')
+})
 module.exports = mongoose.connection
