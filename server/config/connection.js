c4e238dae6890a82a18f0db0fda667fa15abe8e5
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-20 11:56:04 -0500

    connection issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bitstucker:p0rtfo1iOv1@cluster0.hvkzmuy.mongodb.net/')
 
 module.exports = mongoose.connection
