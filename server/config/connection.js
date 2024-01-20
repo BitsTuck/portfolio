c92b3c456231ed3e9ab1dcfe7e3a79c3f3ff8b98
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-20 11:50:15 -0500

    trying to make Mongo work

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,9 +4,4 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI)
 
-const db=mongoose.connection;
-db.on('error', console.error.bind(console,'MongoDB connection error'));
-db.once('open', () => {
-    console.log('Connected to MongoDB Atlas')
-})
 module.exports = mongoose.connection
