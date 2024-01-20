2d43e0c4a8d4dcd125c175efe31feaab3b58ea01
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2024-01-20 11:43:50 -0500

    connection work

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bitstucker:p0rtfo1iOv1@cluster0.hvkzmuy.mongodb.net/')
+mongoose.connect(process.env.MONGODB_URI)
 
 const db=mongoose.connection;
 db.on('error', console.error.bind(console,'MongoDB connection error'));
